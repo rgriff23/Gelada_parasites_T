046cb262af776f97173faff06c6329c7139710bb
rgriff23 <rgriff23@gmail.com>
2017-02-05 12:10:34 +0900

    modify variables for models

--- analysis.R
+++ analysis.R
@@ -1,10 +1,43 @@
-################
+#################
 # PREPARATIONS #
-################
+###############
 
 # Load packages
+library(lme4) # GLMMs
+library(MuMIn) # model comparison
 
 # Read data
 data <- read.csv("~/Desktop/GitHub/Gelada_parasites_T/data.csv")
 
+#####################
+# MODIFY VARIABLES #
+###################
+
+# Cut non adults
+data <- data[data$Status != "NonA",]
+
+# Center age (mean age is 10.6 years)
+data$Age <- data$Age - mean(data$Age)
+
+# Center temperature (midpoint is 13 degrees C)
+data$Temp_midpoint <- data$Temp_midpoint - mean(data$Temp_midpoint)
+
+# New leadership variable
+data$Leader <- ifelse(data$Status=="L", 1, 0)
+
+# Export
+write.csv(data, file="~/Desktop/GitHub/Gelada_parasites_T/data.csv", row.names=F)
+
+###########
+# MODELS #
+#########
+
+# Subset data and remove NAs
+data_sub <- data[,c("Z_t", "Cyst", "Age", "Status", "Z_gc", "Temp_midpoint", "Rain_median_30days", "Name")]
+data_sub <- data_sub[complete.cases(data_sub),]
+
+# GLMM
+mod <- lmer(Z_t ~ Cyst * Age * Z_gc + Temp_midpoint * Rain_median_30days + (1|Name), data=data_sub, na.action=na.fail)
+dredge1 <- dredge(mod)
+model.ave(dredge1)
 
